27166b7e720a2203f62d2a941b6edd0a35ec96c7
root <root@bastion-latamconsultingthi-latamconsultingbrl-j4dhqnaw.srv.ravcl>
2019-09-25 22:56:33 -0400

    alteração

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ def configure_database
   puts "Connected to database"
   puts "Create database..."
   %x"rake db:create"
-  puts "Run migrations..."
+  puts "Run migration..."
   %x"rake db:migrate"
 end
 
